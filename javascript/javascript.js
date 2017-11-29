f5c6867043330b80824a83adedea0a74a56f06ed
issa98 <33475314+issa98@users.noreply.github.com>
2017-11-29 14:43:19 -0500

    Update javascript.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -30,6 +30,43 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
+function login() {
+  document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_login";  
+  document.querySelector('.cont_form_login').style.display = "block";
+  document.querySelector('.cont_form_sign_up').style.opacity = "0";               
+
+  setTimeout(function(){  document.querySelector('.cont_form_login').style.opacity = "1"; },200);  
+  
+  setTimeout(function(){    
+  document.querySelector('.cont_form_sign_up').style.display = "none";
+  },200);  
+  }
+
+function signup() {
+  document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_sign_up";
+  document.querySelector('.cont_form_sign_up').style.display = "block";
+  document.querySelector('.cont_form_login').style.opacity = "0";
+  
+  setTimeout(function(){  document.querySelector('.cont_form_sign_up').style.opacity = "1";},100);  
+
+  setTimeout(function(){   document.querySelector('.cont_form_login').style.display = "none";},400);  
+
+
+}    
+
+
+function login_signup() {
+
+document.querySelector('.cont_forms').className = "cont_forms";  
+document.querySelector('.cont_form_sign_up').style.opacity = "0";               
+document.querySelector('.cont_form_login').style.opacity = "0"; 
+
+setTimeout(function(){
+document.querySelector('.cont_form_sign_up').style.display = "none";
+document.querySelector('.cont_form_login').style.display = "none";},400);  
+}
+
+
 /*Featured Item Slider*/
 var translateX = 0;
 
