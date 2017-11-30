380c44a5faa3f34ac498ea3939c6d7dfcae4c81c
issa98 <33475314+issa98@users.noreply.github.com>
2017-11-30 04:47:54 -0500

    Update javascript1.js

--- javascript/javascript1.js
+++ javascript/javascript1.js
@@ -25,24 +25,3 @@ function hideSubMenu(el){
     el.classList.remove('show-dropMenu');
 }
 
-var index = 0;
-displaySlides();
-
-function displaySlides() {
-var slides = document.getElementsByClassName("store_photos");
-var dots = document.getElementsByClassName("dots");
-for (var i = 0; i < slides.length; i++) {
-slides[i].style.display = "none"; 
-}
-index++;
-if (index > slides.length) {
-index = 1
-} 
-for (i = 0; i < dots.length; i++) {
-dots[i].className = dots[i].className.replace(" active-dot", "");
-}
-slides[index-1].style.display = "block"; 
-dots[index-1].className += " active-dot";
-setTimeout(displaySlides, 4000); // Change image every 4 seconds
-object.style.transition
-}
